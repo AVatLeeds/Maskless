bfb1c0aa7fb11bd149ece7ff7989a1d4f95baf86
Arthur Vie <el20a2v@leeds.ac.uk>
2024-02-03 15:31:23 +0000

    working code to open the PNG image and display in a window

--- png_test.cpp
+++ png_test.cpp
@@ -7,6 +7,8 @@
 
 #include <png.h>
 
+#include "XCB_framebuffer_window.h"
+
 int main(int argc, char * argv[])
 {
     std::cout << "Compiled with libpng version " << PNG_LIBPNG_VER_STRING << ". Using libpng " << png_libpng_ver << ".\n";
@@ -52,11 +54,40 @@ int main(int argc, char * argv[])
 
     png_set_sig_bytes(png_struct_ptr, header_length_bytes);
 
-    png_read_png(png_struct_ptr, png_info_ptr, PNG_TRANSFORM_SCALE_16 | PNG_TRANSFORM_STRIP_ALPHA, NULL);
+    png_read_png(png_struct_ptr, png_info_ptr, PNG_TRANSFORM_SCALE_16 | PNG_TRANSFORM_STRIP_ALPHA | PNG_TRANSFORM_BGR, NULL);
 
-    uint32_t width = png_get_image_width(png_struct_ptr, png_info_ptr);
-    uint32_t height = png_get_image_height(png_struct_ptr, png_info_ptr);
+    unsigned int image_width = png_get_image_width(png_struct_ptr, png_info_ptr);
+    unsigned int image_height = png_get_image_height(png_struct_ptr, png_info_ptr);
+    unsigned int bit_depth = png_get_bit_depth(png_struct_ptr, png_info_ptr);
+    unsigned int row_bytes = png_get_rowbytes(png_struct_ptr, png_info_ptr);
 
-    std::cout << "Image has dimensions " << width << " x " << height << std::endl;
+    std::cout << "Image has dimensions " << image_width << " x " << image_height << std::endl;
+    std::cout << "Image bit depth is " << bit_depth << std::endl;
+    std::cout << "Image has " << row_bytes << " bytes per row." << std::endl;
+
+    png_byte ** row_pointers;
+    row_pointers = png_get_rows(png_struct_ptr, png_info_ptr);
+
+    struct window_props win_props;
+    class Framebuffer_window test_window(image_width, image_height, "PNG preview", 11, &win_props);
+
+    uint8_t * pix_component_ptr;
+    for (int j = 0; j < image_height; j ++)
+    {
+        pix_component_ptr = test_window.framebuffer_ptr + (j * win_props.stride);
+        for (int i = 0; i < (image_width * 3); i += 3)
+        {   
+            *(pix_component_ptr ++) = row_pointers[j][i + 0];
+            *(pix_component_ptr ++) = row_pointers[j][i + 1];
+            *(pix_component_ptr ++) = row_pointers[j][i + 2];
+            *(pix_component_ptr ++) = 0;
+        }
+    }
+
+    while (true)
+    {
+        if (test_window.handle_events() < 0) return 0;
+    }
+
     return 0;
 }
