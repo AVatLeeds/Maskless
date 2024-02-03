e0953854eac662daabb97b818ac00799c15257fd
Arthur Vie <el20a2v@leeds.ac.uk>
2024-02-03 16:42:33 +0000

    merged PNG reading library

---
+++ PNG_reader.h
@@ -0,0 +1,28 @@
+#ifndef PNG_READER_H
+#define PNG_READER_H
+
+#include <cstdint>
+#include <cstdio>
+
+#include <png.h>
+
+class PNG
+{
+    public:
+    PNG();
+    ~PNG();
+
+    int open(char * path);
+    unsigned int width();
+    unsigned int height();
+    unsigned int row_bytes();
+
+    uint8_t **  data_ptr;
+
+    private:
+    FILE * file_ptr;
+    png_struct * png_read_struct_ptr;
+    png_info * png_info_ptr;
+};
+
+#endif
